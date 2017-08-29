2cbbc1e8a81765dc83ba570b2d2860da36da3dc6
Kallum Chumber <kallum_chumber@hotmail.com>
2017-08-29 22:11:06 +0100

    search tree
    search tree

---
+++ Binary Search Tree/Binary Search Tree/BSTree.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_Search_Tree
+{
+    class BSTree<T> : BinTree<T> where T : IComparable
+    { // root declared as protected in Parent Class - BinTree
+        public BSTree()
+        {
+            root = null;
+        }
+
+        public void InsertItem(T item)
+        {
+            insertItem(item, ref root);
+        }
+
+        private void insertItem(T item, ref Node<T> tree)
+        {
+            if (tree == null)
+            {
+                tree = new Node<T>(item);
+            }
+
+            else if (item.CompareTo(tree.Data) < 0)
+            {
+                insertItem(item, ref tree.Left);
+            }
+
+            else if (item.CompareTo(tree.Data) > 0)
+            {
+                insertItem(item, ref tree.Right);
+            }
+        }
+
+        public int Height()
+        // Return the max level of the tree
+        {
+            return height(ref this.root);
+        }
+
+        private int height(ref Node<T> tree)
+        {
+            if (tree == null)
+            {
+                return 0;
+            }
+
+            else if (tree.Left == null && tree.Right == null)
+            {
+                return 0;
+            }
+
+            else
+            {
+                return 1 + max(height(ref tree.Left), height(ref tree.Right));
+            }
+        }
+
+        public int max(int x, int y)
+        {
+            if (x.CompareTo(y) > 0)
+            {
+                return x;
+            }
+
+            else
+            {
+                return y;
+            }
+        }
+
+        public Boolean Contains(T item)
+        // Return true if the item is contained is in the BSTree, false otherwise
+        {
+            return contains(ref this.root, item);
+        }
+
+        private Boolean contains(ref Node<T> tree, T item)
+        {
+            if (tree == null)
+            {
+                return false;
+            }
+
+            else if (tree.Data.Equals(item))
+            {
+                return true;
+            }
+
+            else if (item.CompareTo(tree.Data) < 0)
+            {
+                return contains(ref tree.Left, item);
+            }
+
+            else
+            {
+                return contains(ref tree.Right, item);
+            }
+        }
+
+        public void RemoveItem(T item)
+        {
+            if (Contains(item) == true)
+            {
+                removeItem(ref this.root, item);
+            }
+
+            else
+            {
+                Console.WriteLine("{0} not found", item);
+            }
+        }
+
+        private void removeItem(ref Node<T> tree, T item)
+        {
+            if (tree != null)
+            {
+                if (item.CompareTo(tree.Data) < 0)
+                {
+                    removeItem(ref tree.Left, item);
+                }
+
+                else if (item.CompareTo(tree.Data) > 0)
+                {
+                    removeItem(ref tree.Right, item);
+                }
+
+                else
+                {
+                    if (tree.Left == null && tree.Right == null) // no children
+                    {
+                        tree = null;
+                    }
+
+                    else if (tree.Left != null && tree.Right == null) // left child only
+                    {
+                        tree = tree.Left;
+                    }
+
+                    else if (tree.Left == null && tree.Right != null) // right child only
+                    {
+                        tree = tree.Right;
+                    }
+
+                    else // 2 children
+                    {
+                        if (tree.Right.Left == null)
+                        {
+                            tree.Right.Left = tree.Left;
+                            tree = tree.Right;
+                        }
+
+                        else
+                        {
+                            Node<T> x = tree.Right;
+                            Node<T> y = tree.Right;
+
+                            while (y.Left.Left != null)
+                            {
+                                y = y.Left;
+                            }
+
+                            x = x.Left;
+                            x.Left = y.Right;
+                            y.Left = tree.Left;
+                            y.Right = tree.Right;
+                            tree = y;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Binary Search Tree/Binary Search Tree/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_Search_Tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Creating BSTree1");
+            BSTree<int> myTree1 = new BSTree<int>();
+            myTree1.InsertItem(1);
+            myTree1.InsertItem(5);
+            myTree1.InsertItem(2);
+            myTree1.InsertItem(4);
+            myTree1.InsertItem(6);
+            myTree1.InsertItem(12);
+            myTree1.InsertItem(9);
+            myTree1.InsertItem(13);
+            Console.WriteLine("Done\n");
+
+            Console.WriteLine("Creating BSTree2");
+            BSTree<int> myTree2 = new BSTree<int>();
+            Console.WriteLine("Done\n");
+
+            Console.WriteLine("Height() test");
+            Console.WriteLine("BSTree1 height: {0}", myTree1.Height());
+            Console.WriteLine("BSTree2 height: {0}", myTree2.Height());
+
+            Console.WriteLine();
+
+            Console.WriteLine("Contains() test");
+            Console.WriteLine("BSTree1 contains 4: {0}", myTree1.Contains(4));
+            Console.WriteLine("BSTree1 contains 10: {0}", myTree1.Contains(10));
+
+            Console.WriteLine();
+
+            Console.WriteLine("RemoveItem() test");
+            Console.WriteLine("Removing 4 from BSTree1");
+            myTree1.RemoveItem(4);
+            Console.WriteLine("BSTree1 contains 4: {0}", myTree1.Contains(4));
+
+            Console.ReadKey();
+        }
+    }
+}
